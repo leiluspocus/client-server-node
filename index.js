5cc4005c83ef76044442133400094578f9ca7753
leiluspocus <laila.atrmouh@gmail.com>
2020-04-10 19:14:23 +0200

    :tada: trigger error on server side

--- index.js
+++ index.js
@@ -7,4 +7,5 @@ app.get('/', function (req, res) {
 
 app.listen(4444, function () {
   console.log('App démarrée au port 4444!');
+  alert('Je suis du Javascript qui est exécuté côté client uniquement. Si tu me mets côté serveur, je ne marcherai pas ! ^^');
 })
